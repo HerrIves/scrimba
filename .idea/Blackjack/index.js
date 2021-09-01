142463925729afb200e745b4bc031e44c0716bf2
ivev <spechtator@gmail.com>
2021-09-01 18:30:33 +0300

    second task

--- .idea/Blackjack/index.js
+++ .idea/Blackjack/index.js
@@ -6,3 +6,12 @@ let firstCard = getCard();
 let secondCard = getCard();
 let sum = firstCard + secondCard;
 
+console.log(sum);
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21){
+    console.log("Wohoo! You've got Blackjack!")
+} else {
+    console.log("You're out of the game! 😭")
+}
