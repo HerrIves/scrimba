00763222f751ad1d4e330a9835bdc352dda94a8a
ivev <spechtator@gmail.com>
2021-09-21 17:36:56 +0200

    practice time 02

--- .idea/Blackjack/practice.js
+++ .idea/Blackjack/practice.js
@@ -1,10 +1,20 @@
 let person = {
     name:"Evgeny",
-    age:35,
+    age:3,
     country:"Deutschland",
     logdata: function(){
-        return person.name + "is " + person.age + " years old and lives in " + person.country;
+        return person.name + " is " + person.age + " years old and he lives in " + person.country;
+    },
+    discountF:function(){
+        let discount = "full price";
+
+        if(person.age < 6){discount = "child dicount"
+        }else if(person.age < 26){discount = "student dicount"
+        }else if(person.age > 66) {discount = "senior dicount"
+        }
+        return discount;
     }
 }
 
 console.log(person.logdata());
+console.log(person.discountF())
