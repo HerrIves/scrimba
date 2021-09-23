a34db3fade73322acaf7687281bdb613ee4b40bd
ivev <spechtator@gmail.com>
2021-09-23 11:00:55 +0200

    practice time 03

--- .idea/Blackjack/practice.js
+++ .idea/Blackjack/practice.js
@@ -1,20 +1,38 @@
 let person = {
-    name:"Evgeny",
-    age:3,
-    country:"Deutschland",
-    logdata: function(){
+    name: "Evgeny",
+    age: 3,
+    country: "Deutschland",
+    logdata: function () {
         return person.name + " is " + person.age + " years old and he lives in " + person.country;
     },
-    discountF:function(){
+    discountF: function () {
         let discount = "full price";
 
-        if(person.age < 6){discount = "child dicount"
-        }else if(person.age < 26){discount = "student dicount"
-        }else if(person.age > 66) {discount = "senior dicount"
+        if (person.age < 6) {
+            discount = "child dicount"
+        } else if (person.age < 26) {
+            discount = "student dicount"
+        } else if (person.age > 66) {
+            discount = "senior dicount"
         }
         return discount;
     }
 }
 
 console.log(person.logdata());
-console.log(person.discountF())
+console.log(person.discountF());
+
+let largeCountries = [
+    "China",
+    "India",
+    "USA",
+    "Indonesia",
+    "Pakistan"
+]
+
+function outputCountries() {
+    for (let i = 0; i < largeCountries.length; i++) {
+        console.log(largeCountries[i]);
+    }
+}
+outputCountries();
