0baa91850da77056d7d4d58c92dd1bd5801198af
ivev <spechtator@gmail.com>
2021-10-05 12:07:20 +0200

    15 ul.inner HTML

--- 03 Build a Chrome Extension/index.js
+++ 03 Build a Chrome Extension/index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el");
@@ -7,5 +7,14 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    console.log(myLeads)
+    console.log(myLeads);
+    renderUl();
 })
+
+// Render the leads in the unordered list using ulEl.textContent
+function renderUl(){
+    ulEl.innerText = "";
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+}
