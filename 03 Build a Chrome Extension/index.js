6244686c0e48c5899d3b3569223fcbcc49780848
ivev <spechtator@gmail.com>
2021-10-14 21:32:13 +0200

    21f render function

--- 03 Build a Chrome Extension/index.js
+++ 03 Build a Chrome Extension/index.js
@@ -1,25 +1,18 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el");
-
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    console.log(myLeads);
-    renderUl();
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-let listItem = "";
-
-function renderUl(){
-
-for (let i = 0; i < myLeads.length; i++) {
-// 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItem += "<li>" + myLeads[i] + "</li>";
-}
-// 3. Render the listItems inside the unordered list using ulEl.innerHTML
-    ulEl.innerHTML = listItem;
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
